4febe99d6cf4343a1ad9d3b23be59bcc58d3da88
MatheusGraciki <matheusgraciki7@gmail.com>
2025-08-26 11:37:14 -0300

    Add: navbar css style

--- Portifolio/src/components/Navbar.jsx
+++ Portifolio/src/components/Navbar.jsx
@@ -51,14 +51,6 @@ export const Navbar = () => {
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.95 }}
                 >
-                    <a>About</a>
-                </motion.li>
-
-                <motion.li 
-                    variants={fadeInUp}
-                    whileHover={{ scale: 1.1 }}
-                    whileTap={{ scale: 0.95 }}
-                >
                     <a>Projects</a>
                 </motion.li>
 

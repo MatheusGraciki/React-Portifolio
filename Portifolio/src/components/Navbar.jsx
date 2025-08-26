ef45abc800a4b716e99f433ed611ae6cc7feef79
MatheusGraciki <matheusgraciki7@gmail.com>
2025-08-26 11:25:27 -0300

    Add: more navbar animations

--- Portifolio/src/components/Navbar.jsx
+++ Portifolio/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
-import { motion } from 'framer-motion'
+import { animate, motion } from 'framer-motion'
+
+const fadeInUp ={
+    initial: { opacity: 0, y: 20 },
+    animate: { opacity: 1, y: 0 },
+    transition: { duration: 0.6 },
+};
+
+const staggerContainer = {
+    animate: {
+        transition: {
+            staggerChildren: 0.1,
+        },
+    },
+}
 
 export const Navbar = () => {
     return (
@@ -20,37 +34,38 @@ export const Navbar = () => {
 
             <motion.ul 
                 className='nav-links'
-                animate ={{ transition: { staggerChildren: 0.1 } }}
-            
+                variants={staggerContainer}
+                initial="initial"
+                animate="animate"
             >
                 <motion.li 
-                    initial ={{ opacity: 0, y: 20 }}
-                    animate ={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.6 }}
+                    variants={fadeInUp}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
                 >
                     <a>Home</a>
                 </motion.li>
 
                 <motion.li 
-                    initial ={{ opacity: 0, y: 20 }}
-                    animate ={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
+                    variants={fadeInUp}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
                 >
                     <a>About</a>
                 </motion.li>
 
                 <motion.li 
-                    initial ={{ opacity: 0, y: 20 }}
-                    animate ={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
+                    variants={fadeInUp}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
                 >
                     <a>Projects</a>
                 </motion.li>
 
                 <motion.li 
-                    initial ={{ opacity: 0, y: 20 }}
-                    animate ={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.6, ease: "easeOut" }}
+                    variants={fadeInUp}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.95 }}
                 >
                     <a>Contact</a>
                 </motion.li>

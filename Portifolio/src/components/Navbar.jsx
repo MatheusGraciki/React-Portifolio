6ddd194fd658f01a3bd94282b02f69021e1e2124
MatheusGraciki <matheusgraciki7@gmail.com>
2025-08-26 21:53:59 -0300

    Chore: fix wrong words

--- Portifolio/src/components/Navbar.jsx
+++ Portifolio/src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export const Navbar = () => {
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
             >
-                Portifólio
+                Portfolio
             </motion.div>
 
             <motion.ul 
